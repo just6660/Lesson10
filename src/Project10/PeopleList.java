2e9e7959a59f981758daf2dd191043ff8e4cd71c
just6660 <just6660@ISS2TZQKH2.scdsb.pri>
2020-11-09 07:59:10 -0500

    stuffs

--- src/Project10/PeopleList.java
+++ src/Project10/PeopleList.java
@@ -9,13 +9,83 @@ package Project10;
  *
  * @author just6660
  */
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
+
 public class PeopleList extends javax.swing.JFrame {
 
     /**
      * Creates new form PeopleList
      */
+    ArrayList<Person> people = new ArrayList();
+    DefaultListModel list = new DefaultListModel();
+
     public PeopleList() {
         initComponents();
+        people.add(new Person("Bob", 25, "M"));
+        people.add(new Person("Fran", 55, "F"));
+        people.add(new Person("Mike", 15, "M"));
+        people.add(new Person("Sue", 30, "F"));
+        lstpeople.setModel(list);
+        for (Person p : people) {
+            list.addElement(p.getName());
+        }
+    }
+
+    public static int findInsertPoint(ArrayList a, Object searchValue) {
+        int left = 0;
+        int right = a.size() - 1;
+        int midpoint = 0;
+        int result = 0;
+
+        while (left <= right) {
+            midpoint = (left + right) / 2;
+            result = ((Comparable) a.get(midpoint)).compareTo(searchValue);
+
+            if (result < 0) {
+                left = midpoint + 1;
+            } else {
+                right = midpoint - 1;
+            }
+        }
+        if (result < 0) {
+            midpoint++;
+        }
+        return midpoint;
+    }
+
+    public static int search(ArrayList a, Object searchValue) {
+        int left = 0;
+        int right = a.size() - 1;
+        while (left <= right) {
+            int midpoint = (left + right) / 2;
+            int result = ((Comparable) a.get(midpoint)).compareTo(searchValue);
+            if (result == 0) {
+                return midpoint;
+            } else if (result < 0) {
+                left = midpoint + 1;
+            } else {
+                right = midpoint - 1;
+            }
+        }
+        return -1;
+
+    }
+
+    public void clearForm() {
+        txtname.setText("");
+        txtage.setText("");
+        buttonGroup1.clearSelection();
+        lstpeople.clearSelection();
+    }
+    public void show(Person p){
+        txtname.setText(p.getName());
+        txtage.setText(""+p.getAge());
+        if(p.getGender()== "M")
+            optMale.setSelected(true);
+        else
+            optFemale.setSelected(true);
     }
 
     /**
@@ -35,8 +105,8 @@ public class PeopleList extends javax.swing.JFrame {
         txtname = new javax.swing.JTextField();
         txtage = new javax.swing.JTextField();
         jPanel1 = new javax.swing.JPanel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
+        optMale = new javax.swing.JRadioButton();
+        optFemale = new javax.swing.JRadioButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         mnuExit = new javax.swing.JMenuItem();
@@ -66,11 +136,13 @@ public class PeopleList extends javax.swing.JFrame {
 
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
-        buttonGroup1.add(jRadioButton1);
-        jRadioButton1.setText("Male");
+        buttonGroup1.add(optMale);
+        optMale.setText("Male");
+        optMale.setActionCommand("M");
 
-        buttonGroup1.add(jRadioButton2);
-        jRadioButton2.setText("Female");
+        buttonGroup1.add(optFemale);
+        optFemale.setText("Female");
+        optFemale.setActionCommand("F");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -78,9 +150,9 @@ public class PeopleList extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(24, 24, 24)
-                .addComponent(jRadioButton1)
+                .addComponent(optMale)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
-                .addComponent(jRadioButton2)
+                .addComponent(optFemale)
                 .addGap(20, 20, 20))
         );
         jPanel1Layout.setVerticalGroup(
@@ -88,14 +160,14 @@ public class PeopleList extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(33, 33, 33)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jRadioButton2)
-                    .addComponent(jRadioButton1))
+                    .addComponent(optFemale)
+                    .addComponent(optMale))
                 .addContainerGap(44, Short.MAX_VALUE))
         );
 
         jMenu1.setText("File");
 
-        mnuExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InsertRemove/exit.png"))); // NOI18N
+        mnuExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Project10/exit.png"))); // NOI18N
         mnuExit.setText("Exit");
         mnuExit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -108,32 +180,62 @@ public class PeopleList extends javax.swing.JFrame {
 
         jMenu2.setText("Edit");
 
-        mnuClear.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InsertRemove/exit.png"))); // NOI18N
+        mnuClear.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Project10/exit.png"))); // NOI18N
         mnuClear.setText("Clear");
+        mnuClear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuClearActionPerformed(evt);
+            }
+        });
         jMenu2.add(mnuClear);
 
-        mnuAdd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InsertRemove/insert.png"))); // NOI18N
+        mnuAdd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Project10/insert.png"))); // NOI18N
         mnuAdd.setText("Add");
+        mnuAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuAddActionPerformed(evt);
+            }
+        });
         jMenu2.add(mnuAdd);
 
-        mnuDelete.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InsertRemove/delete.png"))); // NOI18N
+        mnuDelete.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Project10/delete.png"))); // NOI18N
         mnuDelete.setText("Delete");
+        mnuDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuDeleteActionPerformed(evt);
+            }
+        });
         jMenu2.add(mnuDelete);
 
         jMenuBar1.add(jMenu2);
 
         jMenu3.setText("Filter");
 
-        mnuAll.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InsertRemove/all.png"))); // NOI18N
+        mnuAll.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Project10/all.png"))); // NOI18N
         mnuAll.setText("Show All");
+        mnuAll.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuAllActionPerformed(evt);
+            }
+        });
         jMenu3.add(mnuAll);
 
-        mnuFemale.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InsertRemove/female.png"))); // NOI18N
+        mnuFemale.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Project10/female.png"))); // NOI18N
         mnuFemale.setText("Female");
+        mnuFemale.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuFemaleActionPerformed(evt);
+            }
+        });
         jMenu3.add(mnuFemale);
 
-        mnuMale.setIcon(new javax.swing.ImageIcon(getClass().getResource("/InsertRemove/male.png"))); // NOI18N
+        mnuMale.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Project10/male.png"))); // NOI18N
         mnuMale.setText("Male");
+        mnuMale.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuMaleActionPerformed(evt);
+            }
+        });
         jMenu3.add(mnuMale);
 
         jMenuBar1.add(jMenu3);
@@ -189,10 +291,88 @@ public class PeopleList extends javax.swing.JFrame {
     }//GEN-LAST:event_mnuExitActionPerformed
 
     private void lstpeopleMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lstpeopleMouseClicked
-        // TODO add your handling code here:
-        
+        String name = "" + lstpeople.getSelectedValue();
+        int loc = search(people, new Person(name,0,""));
+        show(people.get(loc));
+
         //put code here for clikcing on a name
     }//GEN-LAST:event_lstpeopleMouseClicked
+
+    private void mnuDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuDeleteActionPerformed
+
+        int i = lstpeople.getSelectedIndex();
+        String n = people.get(i).getName();
+        Person temp = new Person(n, 0, null);
+        int loc = search(people, temp);
+        list.removeElementAt(lstpeople.getSelectedIndex());
+        people.remove(loc);
+        clearForm();
+
+
+    }//GEN-LAST:event_mnuDeleteActionPerformed
+
+    private void mnuAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuAddActionPerformed
+        list.clear();
+        for (Person p : people) {
+            list.addElement(p.getName());
+        }
+        String nm, gen;
+        int age;
+        try {
+            nm = txtname.getText();
+            age = Integer.parseInt(txtage.getText());
+            gen = buttonGroup1.getSelection().getActionCommand();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Must fill out form correctly");
+            return;
+
+        }
+        Person temp = new Person(nm,age,gen);
+        int loc = search(people,temp);
+        if(loc==-1){
+            int newloc = findInsertPoint(people,temp);
+            people.add(newloc,temp);
+            list.add(newloc, temp.getName());
+            
+        }
+        else{
+           JOptionPane.showMessageDialog(this,"Person must have a unique name"); 
+        }
+    }//GEN-LAST:event_mnuAddActionPerformed
+
+    private void mnuAllActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuAllActionPerformed
+        list.clear();
+        for (Person p : people) {
+            list.addElement(p.getName());
+        }
+       
+    }//GEN-LAST:event_mnuAllActionPerformed
+
+    private void mnuFemaleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuFemaleActionPerformed
+        // TODO add your handling code here:
+        list.clear();
+        for(Person p : people){
+            if(p.getGender()=="F"){
+                list.addElement(p.getName());
+            }
+        }
+       
+    }//GEN-LAST:event_mnuFemaleActionPerformed
+
+    private void mnuMaleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuMaleActionPerformed
+        // TODO add your handling code here:
+        list.clear();
+        for(Person p : people){
+            if(p.getGender()=="M"){
+                list.addElement(p.getName());
+            }
+        }
+        
+    }//GEN-LAST:event_mnuMaleActionPerformed
+
+    private void mnuClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuClearActionPerformed
+        clearForm();
+    }//GEN-LAST:event_mnuClearActionPerformed
 
     /**
      * @param args the command line arguments
@@ -238,8 +418,6 @@ public class PeopleList extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JList<String> lstpeople;
     private javax.swing.JMenuItem mnuAdd;
@@ -249,6 +427,8 @@ public class PeopleList extends javax.swing.JFrame {
     private javax.swing.JMenuItem mnuExit;
     private javax.swing.JMenuItem mnuFemale;
     private javax.swing.JMenuItem mnuMale;
+    private javax.swing.JRadioButton optFemale;
+    private javax.swing.JRadioButton optMale;
     private javax.swing.JTextField txtage;
     private javax.swing.JTextField txtname;
     // End of variables declaration//GEN-END:variables
--- src/Project10/Person.java
+++ src/Project10/Person.java
@@ -1,6 +1,38 @@
 
 package Project10;
 
-public class Person {
+public class Person implements Comparable{
+    
+    private String name;
+    private int age;
+    private String gender;
+    
+    Person(){
+        name = null;
+        age = 0;
+        gender = null;
+    }
+    Person(String n, int a, String g){
+        name = n;
+        age = a;
+        gender = g;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getGender(){
+        return gender;
+    }
+
+    @Override
+    public int compareTo(Object t) {
+        String other = ((Person)t).getName();
+        return name.compareTo(other);
+    }
+    
     
 }
